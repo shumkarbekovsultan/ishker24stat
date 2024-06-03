d07ac2060580b6ed44cd2f63b3b05339d441f079
shumkarbekovsultan <shumkarbekovsultan@gmail.com>
2024-06-03 15:38:44 +0600

    feat:inital commit

---
+++ src/main/java/com/example/ishker_24_statistic/Ishker24StatisticApplication.java
@@ -0,0 +1,13 @@
+package com.example.ishker_24_statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ishker24StatisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ishker24StatisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ishker_24_statistic/Ishker24StatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ishker_24_statistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ishker24StatisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
